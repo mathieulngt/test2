3731320b7a25b324b556b290a75f16b38cdf34de
mathieu longatte <mlongatte@MBA-MW.local>
2019-02-14 11:17:30 +0100

    import

---
+++ Demande de nombre.py
@@ -0,0 +1,8 @@
+#!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
+
+print("Choisis un nombre inferieure a 100 : ")
+a = float(input())
+if a <=100:
+	print("Bien jouer !")
+else:
+	print("Ce nombre est trop grand :/")
---
+++ dire_bonjour_a.py
@@ -0,0 +1,4 @@
+def dire_bonjour_a(nom):
+	print("Salut " + nom )
+dire_bonjour_a("Bob")
+dire_bonjour_a("Tom")
