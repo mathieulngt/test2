74d99a62e22fbb5ce58e872232c7a58b99d42ebb
mathieu longatte <mlongatte@MBA-MW.local>
2019-02-15 15:16:36 +0100

    Commit du 15/02 15h15 sur A_Terminer

---
+++ A_terminer/Savoir si qq est malde.py
@@ -0,0 +1,3 @@
+print("Es-tu malade ?  oui / non")
+a = float(input(""))
+
---
+++ A_terminer/Savoir si un nombre est impair ou nom.py
@@ -0,0 +1,9 @@
+def est_impair(nombre):
+	if (nombre % 2 == 0):
+		return "true"
+	else:
+		return "false"
+
+nombre = float(input())
+
+est_impair(9)
